3c28d18784acfe2fab1b636baec43e21b2867fd5
Kerlin Michel <michelkerlin@gmail.com>
2025-07-31 16:34:38 -0700

    create do resources; ship concept -> fleet concept

--- port.py
+++ port.py
@@ -21,9 +21,10 @@ configure_parser.add_argument('-n', '--name', required=True)
 
 configure_action_subparser = configure_parser.add_subparsers(dest='configure-action')
 
-configure_add_ship_parser = configure_action_subparser.add_parser('add-ship')
+configure_add_ship_parser = configure_action_subparser.add_parser('add-fleet')
 configure_add_ship_parser.add_argument('-p', '--pier', required=True)
-configure_add_ship_parser.add_argument('ship')
+configure_add_ship_parser.add_argument('-k', '--ssh-key-fp', default="local")
+configure_add_ship_parser.add_argument('fleet')
 
 configure_add_pier_parser = configure_action_subparser.add_parser('add-pier')
 configure_add_pier_parser.add_argument('pier')
@@ -65,23 +66,64 @@ if args.command == 'create':
         s3_client.put_object(Body=json.dumps(_DEFAULT_PORT_CONFIG),
                              Bucket='enfra_ports',
                              Key=f'{args.name}.json')
+
+        pydo_client = utils.create_pydo_client()
+
+        pydo_client.projects.create(
+            body={
+                "name": args.name,
+                "purpose": "Service or API",
+            }
+        )
         print("Port created")
     else:
         print("Port already exists")
 elif args.command == 'configure':
-    if getattr(args, 'configure-action') == 'add-ship':
+    if getattr(args, 'configure-action') == 'add-fleet':
         pydo_client = utils.create_pydo_client()
 
-        load_balancers = pydo_client.load_balancers.list()['load_balancers']
-        lb_exists = False
-        for lb in load_balancers:
-            if lb['name'] == args.ship:
-                lb_exists = True
-        if lb_exists is False:
-            raise ValueError()
+        ship_tag = f"{args.name}-{args.pier}-{args.fleet}-ship"
 
-        config_json['ships'][args.ship] = {"pier": args.pier}
-        print("Added or updated ship")
+        if args.ssh_key_fp == "local":
+            args.ssh_key_fp = utils.get_local_machine_ssh_key_fingerprint()
+
+        asp_resp = pydo_client.autoscalepools.create(
+            body={
+                "name": f"{args.name}-{args.pier}-{args.fleet}",
+                "config": {
+                    "min_instances": 1,
+                    "max_instances": 2,
+                    "target_cpu_utilization": 0.5,
+                },
+                "droplet_template": {
+                    "name": args.fleet,
+                    "region": args.region,
+                    "image": "ubuntu-25-04-x64", # TODO: Don't like that I'm hard coding, but can be change manually
+                    "size": "s-1vcpu-1gb", # TODO: Don't like that I'm hard coding, but can be change manually
+                    "ssh_keys": [args.ssh_key_fp],
+                    "tags": [ship_tag]
+                }
+            }
+        )
+
+        lb_resp = pydo_client.load_balancers.create(
+            body={
+                "name": f"{args.name}-{args.pier}-{args.fleet}",
+                "region": args.region,
+                "forwarding_rules": [
+                    {
+                        "entry_protocol": "http",
+                        "entry_port": 80,
+                        "target_protocol": "http",
+                        "target_port": 80,
+                        "tls_passthrough": False
+                    }
+                ],
+                "tag": ship_tag
+            }
+        )
+
+        print("Added fleet")
     elif getattr(args, 'configure-action') == 'add-pier':
         s3_client.put_object(Body=json.dumps(_DEFAULT_PIER_CONFIG),
                              Bucket='enfra_piers',
